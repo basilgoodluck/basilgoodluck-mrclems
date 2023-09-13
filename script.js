e99249bbc74f45e335aa197007eccbc0437e38cc
Basil Goodluck <105502824+basilgoodluck@users.noreply.github.com>
2023-09-13 09:56:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.getElementsByClassName("fa-bars")
+const exit = document.getElementsByClassName("fa-times")
+const menuItems = document.getElementsByClassName("headerNav")
+
+function show(){
+  
+}
