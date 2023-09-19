c4eebba490045a963eeaa8867b941d958d4e8466
Basil Goodluck <105502824+basilgoodluck@users.noreply.github.com>
2023-09-19 07:21:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ const menuItems = document.getElementsByClassName("headerNav")
 
 show = function (){
   document.getElementById("Menu").style.display = "flex";
-  exit.style.display = "block"
-  // document.getElementsByClassName("fa-bars").style.display = "none"
-  // document.getElementsByClassName("fa-times").style.display = "block"
-}
+  exit.style.display = "block"}
 exit = function(){
   document.getElementById("Menu").style.display = "none";
 }
@@ -19,4 +16,5 @@ toggles.forEach((toggle)=>{
     toggle.parentNode.classList.toggle("active")
   })
 })
+
 
